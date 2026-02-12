9ade9da173cd00123d92fe6bc87b9ca789ff42be
Bread-Baker <aefafach@students.unwsp.edu>
2026-02-12 10:36:25 -0600

    Wrote Program #3: Average Rainfall

--- program_3.py
+++ program_3.py
@@ -1,3 +1,8 @@
+# Name: Ariana Fafach
+# Date: 2/12/2026
+# Title: Program #3: Average Rainfall
+
+
 # Program #3: Average Rainfall
 # Write a program that uses nested loops to collect data and calculate the average 
 # rainfall over a period of years.  
@@ -8,11 +13,47 @@
 # After all iterations, the program should display the number of months, 
 # the total inches of rainfall, and the average rainfall per month for the entire period.
 
-def main():
+
     ######################
-    # WRITE YOUR CODE HERE
+
+# Get the number of years from user:
+years = int(input("Enter the number of years you want to calculate the average rainfall of:  "))
+
+# Set the total inches of rainfall to zero:
+total_rain = 0
+
+# First for loop runs once for every year.
+for i in range(1, years + 1):
+    
+    # Set the year's rain to zero:
+    years_rain = 0
+    
+    # Second for loop runs once for every month.
+    for x in range(1,13):
+        
+        # Get inches of rainfall for every month:
+        rain = int(input(f"Enter the number of inches of rain that fell in month {x} of year {i}:  "))
+        
+        # Add each month's rain fall to the total rainfall for that year:
+        years_rain += rain
+
+    # Add each year's rainfall to the total rainfall of all the years:
+    total_rain += years_rain
+
+# Calculate total number of months:
+total_months = years*12
+
+# Calculate average rainfall per month:
+average_rain = total_rain/total_months
+
+# Display total number of months:
+print(f"The total number of months is {total_months:,}.")
+
+# Display the total rainfall:
+print(f"The total rainfall for the entire period is {total_rain:,} inches.")
+
+# Display the average rainfall per month:
+print(f"The average rain per month is {average_rain:,.2f} inches.")
+
     ######################    
 
-
-if __name__ == '__main__':
-    main()
