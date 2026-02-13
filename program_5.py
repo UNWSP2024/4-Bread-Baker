54adc78237ee091a66b5c5619e0766893f8a2a5c
Bread-Baker <aefafach@students.unwsp.edu>
2026-02-13 12:54:42 -0600

    Wrote Program #5: Bank Balance

--- program_5.py
+++ program_5.py
@@ -1,3 +1,8 @@
+# Name: Ariana Fafach
+# Date: 2/13/2026
+# Title: Program #5: Bank Balance
+
+
 # Program #5: Bank Balance
 # Write a program that asks the user to enter the amount that he or she has budgeted for a month.
 # A loop should then prompt the user to enter each of his or her expenses for the 
@@ -8,11 +13,36 @@
 def main():
     budget = 0.0
     difference = 0.0
-    spent = 1.0         #initialize for while loop
-    total = 0.0
+    spent = 0.0         #initialize for while loop
 
     ######################
-    # WRITE YOUR CODE HERE
+
+    # Get budgeted amount from user:
+    budget = float(input("Enter the amount that you have budgeted for the month:  "))
+    
+    # Set the value of 'expense' equal to anything other than zero to run the loop:
+    expense = 1
+
+    while expense != 0:
+        # Get the expense vaulues from the user:
+        expense = float(input("Enter another expense or 0 to quit:  "))
+        
+        # Accumulate the expenses:
+        spent += expense
+
+    # Calculate the difference between budgeted and spent:
+    difference = budget - spent 
+
+    # Display messages for all different values of 'difference':
+    if difference > 0:
+        print(f"You are under budget by ${difference:,.2f}!")
+    
+    elif difference == 0:
+        print(f"You are right on your budgeted amount.")
+    
+    elif difference < 0:
+        print(f"You are over budget by ${abs(difference):,.2f}.")
+
     ######################
 
 
